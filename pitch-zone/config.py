8b423ef60f77849a0f950a210ee64378e5f35ac5
Kizito-Alberrt <wagabi98@gmail.com>
2021-03-03 12:32:47 +0300

    deployment to heroku

--- pitch-zone/config.py
+++ pitch-zone/config.py
@@ -6,7 +6,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pitch:moringa@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:wagabi@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos' 
     # email configuration
